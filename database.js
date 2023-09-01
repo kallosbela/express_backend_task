5be1f399cd7a66148518dc65708b1390be896c8e
kallosbela <kallosbela@gmail.com>
2023-09-01 10:42:39 +0200

    add mysql url8

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const dotenv = require('dotenv').config();
 
 // MySQL configuration
-const db = mysql.createConnection(`${{express_backend_task.MYSQL_URL}}`);
+const db = mysql.createConnection(`${{express_backend_task.DB_URL}}`);
 
 db.connect((err) => {
   if (err) {
