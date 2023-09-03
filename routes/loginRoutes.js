f5c1783f99cde899f180320a7e2d0dcb97fbe636
kallosbela <kallosbela@gmail.com>
2023-09-03 17:18:48 +0200

    update cikk modified

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken");
 
 router.post("/", async (req, res) => {
   const { user, password } = req.body;
-  console.log("ezt kaptam:", user, password);
   if (user === "pmcode" && password === "titok1234") {
     const jwtToken = jwt.sign({ user }, "secret", { expiresIn: 7200 });
     res.json({ message: "Welcome Back!", token: jwtToken });
